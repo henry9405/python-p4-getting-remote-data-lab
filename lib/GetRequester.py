718b6aea6f39801add56f08442f9318992d823c2
henry9405 <charleshenry9405@gmail.com>
2024-01-15 11:05:55 +0300

    get requester

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,17 +1,3 @@
-# import requests
-# import json
-
-# class GetRequester:
-
-#     def __init__(self, url):
-#         self.url = url
-
-#     def get_response_body(self):
-#         pass
-
-#     def load_json(self):
-#         pass
-
 import requests
 import json
 
